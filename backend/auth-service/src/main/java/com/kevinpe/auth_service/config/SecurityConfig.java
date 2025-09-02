d870d6f11047235d24bf1b65d7d1060199703581
Kevin Pettersson <kevin.petterssons@hotmail.com>
2025-09-02 20:02:31 +0200

    Docker images created, started with docker-compose

--- backend/auth-service/src/main/java/com/kevinpe/auth_service/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/kevinpe/auth_service/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.kevinpe.auth_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
